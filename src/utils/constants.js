a7d0e1d922d84b969119bc419528fd501ffbf6d6
DESKTOP-1JGOD95\APEIRON 2.0 <veljko.mijovic@hypercubesoft.com>
2025-01-29 23:07:59 +0100

    Removing sensitive data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 let constants = {
-    MONGODB_ATLAS_CONN_STRING: "mongodb+srv://komsa:komsamaksa@cluster0.voebp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGODB_ATLAS_CONN_STRING: ""
 }
 
 module.exports = constants;
