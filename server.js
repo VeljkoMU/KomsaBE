8084ae2d91c50a06cf1dc992e6cfe5664854f338
urosesports <uros.maksimovic@carcon.ai>
2025-02-10 19:29:49 +0100

    small fix

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const { mongoDBConnect } = require("./src/db/mongodb");
 const constants = require("./src/utils/constants");
-const authRouter = require("./src/router/authRouter");
+const app = express();
+const authRounter = require("./src/router/authRouter");
 const userRouter = require("./src/router/userRouter");
 const businessRouter = require("./src/router/businessRouter");
-
-const app = express();
 
 app.use(express.json());
 mongoDBConnect(constants.MONGODB_ATLAS_CONN_STRING);
 
-app.use("/auth", authRouter);
+app.use("/auth", authRounter);
 app.use("/user", userRouter);
 app.use("/business", businessRouter);
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server started on port ${PORT}.`);
-});
+app.listen(() => {
+  console.log("Server started.");
+}, process.env.PORT || 3000);
