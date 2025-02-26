007b6110ced0057f6f22811d17c7748873d56524
urosesports <83064029+MaksimovicUros@users.noreply.github.com>
2025-02-26 19:01:41 +0100

    implement chat update and get

--- src/controller/chatController.js
+++ src/controller/chatController.js
@@ -1,56 +1,86 @@
 const jwt = require("jsonwebtoken");
-
+const ChatSingleModel = require("../model/chatSingle");
 class ChatController {
-    static rooms = new Map();
-    
-    static async handleWebSocets(io) {
-       io.on("connection", (socket) => {
-        socket.on("join-room", (data) => {
-            const token = data.split(":")[0];
-            const userId = data.split(":")[1];
-            const room = data.split(":")[2];
-            try {
-                jwt.verify(token, env.PROCCESS.JWT_SECERT);
-                 //TODO Check chat credentials
-                 socket.join(room);
-            }
-            catch(err) {
-                socket.emit("error", {error: err});
-            }
-        });
+  static rooms = new Map();
 
-        socket.on("message", (message) => {
-            const token = message.split(":")[0];
-            const userId = message.split(":")[1]
-            const room = message.split(":")[2];
-            const msg = message.split(":")[3];
-            const chatType = message.split(":")[4];
+  static async handleWebSocets(io) {
+    io.on("connection", (socket) => {
+      socket.on("join-room", (data) => {
+        const token = data.split(":")[0];
+        const userId = data.split(":")[1];
+        const room = data.split(":")[2];
+        try {
+          jwt.verify(token, env.PROCCESS.JWT_SECERT);
+          //TODO Check chat credentials
+          socket.join(room);
+        } catch (err) {
+          socket.emit("error", { error: err });
+        }
+      });
 
-            try {
-                jwt.verify(token, env.PROCCESS.JWT_SECERT);
-                //TODO save message to Chat
-                //TODO Check chat credentials
-                io.to(room).emit("receive", {
-                    senderId: userId,
-                    senderName: "", // To be added
-                    content: msg,
-                    timestamp: Date.now()
-                });
-            }
-            catch(err) {
-                socket.emit("error", {error: err});
-            }
-        });
-       }); 
+      socket.on("message", (message) => {
+        const token = message.split(":")[0];
+        const userId = message.split(":")[1];
+        const room = message.split(":")[2];
+        const msg = message.split(":")[3];
+        const chatType = message.split(":")[4];
+
+        try {
+          jwt.verify(token, env.PROCCESS.JWT_SECERT);
+          //TODO save message to Chat
+          //TODO Check chat credentials
+          io.to(room).emit("receive", {
+            senderId: userId,
+            senderName: "", // To be added
+            content: msg,
+            timestamp: Date.now(),
+          });
+        } catch (err) {
+          socket.emit("error", { error: err });
+        }
+      });
+    });
+  }
+
+  static async getChat(req, res) {
+    const { chatId } = req.params;
+
+    try {
+      const chat = await ChatSingleModel.findOne({ chatId });
+
+      if (!chat) {
+        return res.status(404).json({ error: "Chat not found" });
+      }
+
+      return res.status(200).json(chat.chatsItems);
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: "Error fetching chat data", details: err.message });
     }
+  }
 
-    static async getChat(req, res) {
-        //Impeleted get chat by user's ID
-    }
+  static async updateChat(req, res) {
+    const { id, userIds, chatItems } = req.body;
 
-    static updateChat(req, res) {
-        // Create or update chat by ID
+    try {
+      let chat = await ChatGroupModel.findOne({ id });
+
+      if (chat) {
+        chat.chatItems.push(...chatItems);
+        await chat.save();
+        return res.status(200).json({ message: "Chat updated", chat });
+      } else {
+        chat = new ChatGroupModel({ id, userIds, chatItems });
+        await chat.save();
+        return res.status(201).json({ message: "Chat created", chat });
+      }
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: "Error updating chat", details: err.message });
     }
+  }
 }
 
 module.exports = ChatController;
--- src/model/chatGroup.js
+++ src/model/chatGroup.js
@@ -2,8 +2,9 @@ const { default: mongoose, mongo } = require("mongoose");
 const chatItemSchema = require("./chatItem");
 
 const chatGroupSchema = mongoose.Schema({
-    userIds: [String],
-    chatItems: [chatItemSchema]
+  id: { type: String, index: true },
+  userIds: [String],
+  chatItems: [chatItemSchema],
 });
 
 const ChatGroupModel = mongoose.model("ChatGroup", chatGroupSchema);
